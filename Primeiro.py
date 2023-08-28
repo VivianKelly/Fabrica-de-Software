eab72e1e1c8e0d71e8fd34e5ee463e235605ccdb
VivianKelly <winterplanwt@gmail.com>
2023-08-28 17:04:17 -0300

    Enviando primeira atividade

---
+++ Primeiro.py
@@ -0,0 +1,11 @@
+'''
+1. Crie um programa que peça um número e mostre seu sucessor e antecessor.
+
+'''
+
+numero = int(input("Digite um número:"))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print("O antecessor do número", numero, "é", antecessor, "e seu sucessor é" , sucessor)
